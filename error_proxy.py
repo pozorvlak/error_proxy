f315af5ac9aaadd4eb4300114136889202663c0b
Miles Gould <miles@assyrian.org.uk>
2014-06-06 16:27:58 +0100

    Forward GET requests.

--- error_proxy.py
+++ error_proxy.py
@@ -3,15 +3,22 @@
 import sys
 import json
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from urlparse import urljoin
+from urllib import URLopener
 
 class ErrorHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         print self.path
-        conf = self.config['get']
-        if self.path in conf:
-            self.send_error(conf[self.path])
+        get_conf = self.config['get']
+        if self.path in get_conf:
+            self.send_error(get_conf[self.path])
+        elif "forward_to" in self.config:
+            url = urljoin(self.config['forward_to'], self.path)
+            o = URLopener().open(url)
+            self.wfile.write(o.read())
+            o.close()
         else:
-            self.send_error(conf['*'])
+            self.send_error(get_conf['*'])
 
 if sys.argv[1:]:
     config_file = sys.argv[1:]

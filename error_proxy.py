c0882e8096d1fecd5785a85e43a472d2e6d184db
Miles Gould <miles@assyrian.org.uk>
2014-06-06 15:47:26 +0100

    Configure GET response via Proxyfile

--- error_proxy.py
+++ error_proxy.py
@@ -6,7 +6,7 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class ErrorHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(420)
+        self.send_response(self.config['get_response'])
 
 if sys.argv[1:]:
     config_file = sys.argv[1:]
@@ -14,5 +14,6 @@ else:
     config_file = "Proxyfile"
 with open(config_file) as c:
     config = json.load(c)
+ErrorHTTPRequestHandler.config = config
 httpd = HTTPServer(("localhost", config['port']), ErrorHTTPRequestHandler)
 httpd.serve_forever()

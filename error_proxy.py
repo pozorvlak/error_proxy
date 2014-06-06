d9f3e43a05663706b266d60c1f707133b0c3b6a0
Miles Gould <miles@assyrian.org.uk>
2014-06-06 15:38:59 +0100

    Configure port via a Proxyfile

--- error_proxy.py
+++ error_proxy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import json
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class ErrorHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -8,8 +9,10 @@ class ErrorHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(420)
 
 if sys.argv[1:]:
-    port = sys.argv[1:]
+    config_file = sys.argv[1:]
 else:
-    port = 8000
-httpd = HTTPServer(("localhost", port), ErrorHTTPRequestHandler)
+    config_file = "Proxyfile"
+with open(config_file) as c:
+    config = json.load(c)
+httpd = HTTPServer(("localhost", config['port']), ErrorHTTPRequestHandler)
 httpd.serve_forever()
